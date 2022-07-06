4e1ce0c57b273bda64f645551c85b946f93e8846
Kooreis <kooreis.official@gmail.com>
2022-07-06 07:00:00 -0500

    Initial setup of the program, displaying the options for temperature conversion.

--- Temperature_Converter.cs
+++ Temperature_Converter.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -8,27 +7,5 @@ class Program
         Console.WriteLine("Temperature Converter");
         Console.WriteLine("1. Celsius to Fahrenheit");
         Console.WriteLine("2. Fahrenheit to Celsius");
-        Console.Write("Enter your choice: ");
-        int choice = Convert.ToInt32(Console.ReadLine());
-
-        if (choice == 1)
-        {
-            Console.Write("Enter temperature in Celsius: ");
-            double celsius = Convert.ToDouble(Console.ReadLine());
-            double fahrenheit = (celsius * 9 / 5) + 32;
-            Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
-        }
-        else if (choice == 2)
-        {
-            Console.Write("Enter temperature in Fahrenheit: ");
-            double fahrenheit = Convert.ToDouble(Console.ReadLine());
-            double celsius = (fahrenheit - 32) * 5 / 9;
-            Console.WriteLine("Temperature in Celsius: " + celsius);
-        }
-        else
-        {
-            Console.WriteLine("Invalid choice");
-        }
     }
 }
-```
