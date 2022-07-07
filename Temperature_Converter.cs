dd07ae81b1485d2e29b67b2a3e69751008ad24d7
Kooreis <kooreis.official@gmail.com>
2022-07-07 08:00:00 -0500

    Added user input to choose the type of temperature conversion.

--- Temperature_Converter.cs
+++ Temperature_Converter.cs
@@ -1,11 +1,2 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Temperature Converter");
-        Console.WriteLine("1. Celsius to Fahrenheit");
-        Console.WriteLine("2. Fahrenheit to Celsius");
-    }
-}
+Console.Write("Enter your choice: ");
+int choice = Convert.ToInt32(Console.ReadLine());
