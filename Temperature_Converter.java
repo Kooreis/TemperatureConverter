c2efd3499916a131befb8958f4bb87fc3d60e108
Kooreis <kooreis.official@gmail.com>
2022-07-07 08:00:00 -0500

    Added functionality to prompt the user to enter a temperature in Celsius and read the input.

--- Temperature_Converter.java
+++ Temperature_Converter.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
     }
 }
