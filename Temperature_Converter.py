b5502e6543a4de042330d7658d818e7b2d7c9c64
Kooreis <kooreis.official@gmail.com>
2022-07-07 08:00:00 -0500

    Added user input to select the conversion type.

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,4 +1,1 @@
-def temperature_converter():
-    print("Temperature Converter")
-    print("1. Celsius to Fahrenheit")
-    print("2. Fahrenheit to Celsius")
+choice = int(input("Enter your choice: "))
